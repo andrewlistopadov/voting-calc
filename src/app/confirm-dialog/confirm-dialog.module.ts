c8384c7015fa36f53220a35ebd564fbe5ae57161
“Andrei <andrew.listopadov@gmail.com>
2022-02-04 13:53:58 +0300

    adds confirm dialog;

---
+++ src/app/confirm-dialog/confirm-dialog.module.ts
@@ -0,0 +1,9 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ConfirmDialogComponent} from './confirm-dialog.component';
+
+@NgModule({
+  declarations: [ConfirmDialogComponent],
+  imports: [CommonModule],
+})
+export class ConfirmDialogModule {}
